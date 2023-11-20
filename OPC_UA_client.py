e857ea8d3ab71d950ae50c56b4589e1c033afdc9
WoobieApps <piotrasel@gmail.com>
2023-11-21 00:12:32 +0100

    ADD working Sample Server device

--- OPC_UA_client.py
+++ OPC_UA_client.py
@@ -1,22 +1,22 @@
 from opcua import Client
 import time
 
-def (connect)
 url = "opc.tcp://opcua.demo-this.com:51210/UA/SampleServer"
 
-client = Client(url, timeout = 600)
+def get_data_from_OPC_UA_server():
+    client = Client(url, timeout = 600)
+    output_data = dict()
 
-client.set_encoding_limits(
-    max_string_length=16384,
-    max_array_length=16384,
-    max_byte_string_length=16384,
-    max_message_size=16777216,
-)
+    client.connect()
+    print(f"[OPC-UA]: Client connected to: {url}")
 
-client.connect()
-print("Client connected")
+    node_dynamic_double = client.get_node("ns=2;i=10848")
+    
+    print("[OPC-UA]: Client received data")
 
+    output_data["dynamic_double"] = node_dynamic_double.get_value()
 
-node_val = client.get_node("ns=2;i=10854")
-val = node_val.get_value()
-print(val)
+    client.disconnect()
+    print("[OPC-UA]: Client disconnected")
+
+    return output_data
--- database.py
+++ database.py
@@ -1,27 +1,32 @@
 import mysql.connector
-import time
-import random
 
-insert_query = "INSERT INTO data (variable) VALUES (%s)"
+create_table_query = "CREATE TABLE IF NOT EXISTS data ( id INT AUTO_INCREMENT PRIMARY KEY, \
+                                                        variable INT UNSIGNED)"
+insert_query = "INSERT INTO data (id, variable) VALUES (%s, %s)"
 
-while True:
-    try:
-        db = mysql.connector.connect(
-            host = "mysql.agh.edu.pl",
-            port = 3306, user = "piotrlub", 
-            passwd = "maQESXSwFBJJWjbb", 
-            database = "piotrlub",
-            connect_timeout = 5
-        )
-        cursor = db.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS data (variable DOUBLE(7,3))")
-        print("Connected")
-        value = round(random.uniform(0,1),3)
-        cursor.execute(insert_query, (value,))
-        db.commit()
-        cursor.close()
-        db.close()
-        time.sleep(60)
-    except mysql.connector.errors.InterfaceError as error:
-        print(f"Error {error}")
-    time.sleep(1)
+def send_data_to_database(data_dict):
+    # Try connecting to the database
+    db = mysql.connector.connect(
+        host = "mysql.agh.edu.pl",
+        port = 3306, user = "piotrlub", 
+        passwd = "maQESXSwFBJJWjbb", 
+        database = "piotrlub",
+        connect_timeout = 5
+    )
+    cursor = db.cursor()
+    print("[DB]: Connected to database mysql.agh.edu.pl")
+
+    # Create a data table if needed
+    cursor.execute(create_table_query)
+
+    for key,value in data_dict.items():
+        to_insert = (None,value)
+        cursor.execute(insert_query, to_insert)
+    
+    print("[DB]: New record sent to the database")
+
+    db.commit()
+    cursor.close()
+    db.close()
+
+    print("[DB]: Disonnected from the database")
